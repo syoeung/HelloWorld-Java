2ef617d81a8d8b70399a8c8ebec17adf66836c91
Steven Yoeung <yoeung@outlook.com>
2019-02-14 11:22:37 -0500

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,22 @@
+package com.srccodes.example;
+
+
+/* Hello World for Java */
+
+public class HelloWorld
+ {
+   public static void main(String args[]) {
+   System.out.println("Hello World");
+   
+   String message = "This is a string";
+   System.out.println(message);
+   
+   System.out.println("Hello World" + ". " + message);
+   
+   for (int i=0; i < 10 ; i++ ) {
+   System.out.println(i);
+   }
+  
+}
+
+}
